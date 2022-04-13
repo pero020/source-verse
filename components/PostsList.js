e7d46bd7490c74c2529f495356a8c286cda5d269
pero020 <petar.przic1@gmail.com>
2022-04-13 14:16:03 +0200

    Add date to question list

--- components/PostsList.js
+++ components/PostsList.js
@@ -2,6 +2,7 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 
 const style = {
   width: '100%',
@@ -12,12 +13,19 @@ const style = {
 export default function PostsList(props) {
   const posts = props.posts
   console.log(posts)
+
+  function formatDate(date) {
+    let formatedDate = date.split("T")[0]
+    return(formatedDate)
+  }
+
   return (
     <List sx={style} component="nav" aria-label="mailbox folders">
       {posts.map((post) => (
         <div key={post._id}>
         <ListItem button>
           <ListItemText primary={post.title} secondary={post.author.name} />
+          <Typography variant="p">{formatDate(post.creationDate)}</Typography>
         </ListItem>
         <Divider />
         </div>
