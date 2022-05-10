c1b5b370634030ae2ddaea3495d7c342453ed3b7
Petar <67969157+pero020@users.noreply.github.com>
2022-05-10 10:20:32 +0200

    Setup error 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+export default function Custom404() {
+
+  return <>
+        <Typography>404 - Page not found</Typography>
+  </>
+
+  
+}
