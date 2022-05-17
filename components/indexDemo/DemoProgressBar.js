35ee54c0c8c4f08c04763ac2f73c0acbefca707a
RennSter20 <88338561+RennSter20@users.noreply.github.com>
2022-05-18 00:37:21 +0200

    new layout for learn section pt.2

--- components/indexDemo/DemoProgressBar.js
+++ components/indexDemo/DemoProgressBar.js
@@ -117,14 +117,14 @@ export default function DemoProgressBar() {
       </Container>
 </Stack>
 
-    <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{mr:{md:25}, mt:{md:5}}}>
+    <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{mr:{md:25}, mt:5}}>
       <Typography sx={{mb:2, color:'white'}} variant="body1">Source: <a style={{color: "#42a862", textDecoration: "none"}} href="https://guides.lib.uw.edu/research/faq/reliable">University of Washington</a></Typography>
     </Stack>
 
-    <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
+    <Stack direction="column" alignItems="center" justifyContent="center" spacing={2} sx={{mt:5}}>
       <Typography style={{color:"white"}} variant="h6">Need more help?</Typography> 
       <Typography style={{color:"white"}} variant="h6">Contact our
-      <Button sx={{ml:{xs:0, sm:2}}} href="q&a/private/" variant="contained" color="green">
+      <Button sx={{ml:{xs:2, sm:2}}} href="q&a/private/" variant="contained" color="green">
         Specialists
       </Button></Typography> 
       </Stack>
@@ -132,45 +132,91 @@ export default function DemoProgressBar() {
 
 
       <TabPanel value={value} index={1}>
-        <Typography sx={{mb:4}} variant="h4">10 steps to follow for determining the safety of a website:</Typography>
+ <Container>
+      <Stack direction="row" alignItems="center" justifyContent="center">
+        <Typography sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}} variant="h4">10 steps to follow for determining the safety of a website:</Typography>
+      </Stack>
 
-        <Typography variant="h6">1. Check the SSL certificate</Typography>
-        <Typography sx={{mb:2}} variant="body1">If you only see HTTP within a URL, you should know that the website is not encrypted, meaning your activity could be visible to online predators. Essentially, HTTPS is a security feature provided by an SSL certificate, which is the part of a URL that encrypts a website. This adds a layer of defense against malicious cybercriminals and protects the site's information as it travels from server to server.</Typography>
+<Stack direction="column" alignItems="center" justifyContent="center" spacing={5}>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>1. Check the SSL certificate</Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography sx={{mb:2}} variant="body1" style={{color:"white"}}>If you only see HTTP within a URL, you should know that the website is not encrypted, meaning your activity could be visible to online predators. Essentially, HTTPS is a security feature provided by an SSL certificate, which is the part of a URL that encrypts a website. This adds a layer of defense against malicious cybercriminals and protects the site's information as it travels from server to server.</Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">2. Double-check the domain</Typography>
-        <Typography sx={{mb:2}} variant="body1">Oftentimes, a cybercriminal will create a malicious website and URL that mimics another high-traffic website to trick users into logging in or making a purchase. This could grant the attacker access to private credentials and billing information that they can then use for credential stuffing. They could also decide to sell your info on the dark web to make a profit.</Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>2. Double-check the domain</Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography sx={{mb:2}} variant="body1" style={{color:"white"}}>Oftentimes, a cybercriminal will create a malicious website and URL that mimics another high-traffic website to trick users into logging in or making a purchase. This could grant the attacker access to private credentials and billing information that they can then use for credential stuffing. They could also decide to sell your info on the dark web to make a profit.</Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">3. Search for a privacy policy </Typography>
-        <Typography sx={{mb:2}} variant="body1">The privacy policy outlines for users how the trusted website’s company collects, uses, and protects their data. You'll find that most secure sites have one, since countries such as the U.S., Canada, and Australia sometimes require them by law. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>3. Search for a privacy policy </Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">The privacy policy outlines for users how the trusted website’s company collects, uses, and protects their data. You'll find that most secure sites have one, since countries such as the U.S., Canada, and Australia sometimes require them by law. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">4. Analyze the website design</Typography>
-        <Typography sx={{mb:2}} variant="body1">Cybercriminals often throw together unsecure websites in a short amount of time, ignoring attractive design elements that more popular pages incorporate. Spelling errors and grammar mistakes will likely appear throughout the site as well. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>4. Analyze the website design</Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">Cybercriminals often throw together unsecure websites in a short amount of time, ignoring attractive design elements that more popular pages incorporate. Spelling errors and grammar mistakes will likely appear throughout the site as well. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">5. Verify ownership  </Typography>
-        <Typography sx={{mb:2}} variant="body1">Verifying the owner of a website is great for those learning how to check if a website is secure and it's actually much simpler than many might expect. With the help of Whois Lookup, you can find the name of the registered individual or legal entity that owns the website you're trying to visit. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>5. Verify ownership  </Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">Verifying the owner of a website is great for those learning how to check if a website is secure and it's actually much simpler than many might expect. With the help of Whois Lookup, you can find the name of the registered individual or legal entity that owns the website you're trying to visit. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">6. Find contact information </Typography>
-        <Typography sx={{mb:2}} variant="body1">For some, the presence of contact information can make them feel more comfortable with the website they're on. Recent studies have shown that 44% of website visitors will leave a website if there is no contact information provided. Though this information won’t protect you from dangerous websites, it shows you there is someone to reach out to if you run into security concerns. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>6. Find contact information </Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">For some, the presence of contact information can make them feel more comfortable with the website they're on. Recent studies have shown that 44% of website visitors will leave a website if there is no contact information provided. Though this information won’t protect you from dangerous websites, it shows you there is someone to reach out to if you run into security concerns. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">7. Identify (and question) trust seals </Typography>
-        <Typography sx={{mb:2}} variant="body1">One of the best indicators for those trying to learn how to know if a website is safe is a trust seal. Trust seals are icons with the words “Secure” or “Verified” located beside a URL at the top of the webpage. It’s not enough, however, to just see a trust seal. Nowadays, attackers have found ways to mimic legitimate seals to trick users. Luckily, confirming authentic trust seals is pretty simple — just click on the badge to see if it takes you to a verification page. This confirms that the site is working with a security partner in charge of protecting the data shared and stored on the trusted website. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>7. Identify (and question) trust seals </Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">One of the best indicators for those trying to learn how to know if a website is safe is a trust seal. Trust seals are icons with the words “Secure” or “Verified” located beside a URL at the top of the webpage. It’s not enough, however, to just see a trust seal. Nowadays, attackers have found ways to mimic legitimate seals to trick users. Luckily, confirming authentic trust seals is pretty simple — just click on the badge to see if it takes you to a verification page. This confirms that the site is working with a security partner in charge of protecting the data shared and stored on the trusted website. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">8. Look for reviews  </Typography>
-        <Typography sx={{mb:2}} variant="body1">If you're interested in buying from a company you've never dealt with before, it's best to do a little research. Google the company's name and look into other customer experiences. This form of social proof can help distinguish legitimate businesses from potential scams. </Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>8. Look for reviews  </Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">If you're interested in buying from a company you've never dealt with before, it's best to do a little research. Google the company's name and look into other customer experiences. This form of social proof can help distinguish legitimate businesses from potential scams. </Typography>
+        </Container>
+        </Box>
 
-        <Typography variant="h6">9. Consider cybersecurity tools</Typography>
-        <Typography sx={{mb:2}} variant="body1">Sure, downloading antivirus software is great for people unsure of how to know if a website is safe or not. But the tools don't stop there. A VPN provides a secure and encrypted internet connection for users browsing online. If you’re wondering how to check if a website is safe before entering, there are also website safety checkers you can install that scan and flag suspicious URLs to help identify potentially dangerous websites.</Typography>
+        <Box>
+        <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>9. Consider cybersecurity tools</Typography>
+        <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">Sure, downloading antivirus software is great for people unsure of how to know if a website is safe or not. But the tools don't stop there. A VPN provides a secure and encrypted internet connection for users browsing online. If you’re wondering how to check if a website is safe before entering, there are also website safety checkers you can install that scan and flag suspicious URLs to help identify potentially dangerous websites.</Typography>
+        </Container>
+        </Box>
+        </Stack>
+        <Box sx={{mt:5}}>
+          <Typography variant="h6" sx={{mb:4,color:'background.contrastColor', textDecoration:'underline', textDecorationColor:"#52d17b", display:'inline'}}>10. Know the signs of unsecure websites </Typography>
+            <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
+              <List sx={{ listStyleType: 'disc', ml: 4, mb: 4 }}>
+                <ListItem style={{color:"white"}} sx={{ display: 'list-item' }}>Search engine warnings</ListItem>
+                <ListItem style={{color:"white"}} sx={{ display: 'list-item' }}>Spam</ListItem>
+                <ListItem style={{color:"white"}} sx={{ display: 'list-item' }}>Redirects</ListItem>
+                <ListItem style={{color:"white"}} sx={{ display: 'list-item' }}>Pop-ups</ListItem>
+              </List>
+          </Container>
+          </Box>
 
-        <Typography variant="h6">10. Know the signs of unsecure websites </Typography>
-        <List sx={{ listStyleType: 'disc', ml: 4, mb: 4 }}>
-          <ListItem sx={{ display: 'list-item' }}>Search engine warnings</ListItem>
-          <ListItem sx={{ display: 'list-item' }}>Spam</ListItem>
-          <ListItem sx={{ display: 'list-item' }}>Redirects</ListItem>
-          <ListItem sx={{ display: 'list-item' }}>Pop-ups</ListItem>
-        </List>
-
-        <Typography sx={{mb:2}} variant="body1">Sources: <a style={{color: "#42a862", textDecoration: "none"}} href="https://us.norton.com/internetsecurity-how-to-how-to-know-if-a-website-is-safe.html">us.norton.com</a>, <a style={{color: "#42a862", textDecoration: "none"}} href="https://www.avg.com/en/signal/website-safety">avg.com</a></Typography>
-
+        </Container>
+        <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{mr:{md:50}, mt:5}}>
+        <Typography style={{color:"white"}} sx={{mb:2}} variant="body1">Sources: <a style={{color: "#42a862", textDecoration: "none"}} href="https://us.norton.com/internetsecurity-how-to-how-to-know-if-a-website-is-safe.html">us.norton.com</a>, <a style={{color: "#42a862", textDecoration: "none"}} href="https://www.avg.com/en/signal/website-safety">avg.com</a></Typography>
+        </Stack>
       </TabPanel>
 
       <TabPanel value={value} index={2}>
