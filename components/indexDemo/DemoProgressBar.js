4fcd5016cac5c19b268fe22830847cac8e4bee54
pero020 <petar.przic1@gmail.com>
2022-05-17 22:15:57 +0200

    fix learn alignment trustworthiness

--- components/indexDemo/DemoProgressBar.js
+++ components/indexDemo/DemoProgressBar.js
@@ -67,6 +67,7 @@ export default function DemoProgressBar() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
+      <Container>
 
       <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
       <Typography sx={{mb:2}} variant="body1" style={{color:"white"}}>What it means for a source to be credible/reliable can vary depending on the context of its use. Generally, a credible or reliable source is one that experts in your subject domain would agree is valid for your purposes. This can vary, so it is best to use one of the source evaluation methods that best fits your needs. Do remember that credibility is contextual!</Typography>
@@ -75,7 +76,7 @@ export default function DemoProgressBar() {
       </Container>
 
 <Stack sx={{mt:{md:10, xs:5}}} direction="column" justifyContent="center" alignItems={{md:'flex-start', xs:'center'}}>
-     <Typography sx={{ml:{md:20}}}  variant="h3"><a style={{color:"white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/5ws">5 W Questions (5Ws):</a></Typography>
+     <Typography variant="h3"><a style={{color:"white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/5ws">5 W Questions (5Ws):</a></Typography>
       <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
       <Typography style={{color:"white"}} variant="body1"> This method means thinking critically about each of your sources by answering five questions to determine if the source is credible/reliable. The acceptable answers to these questions will vary depending on your needs. The questions are: </Typography>
       <List sx={{ listStyleType: 'disc', ml: 4, mb: 2 }}>
@@ -89,7 +90,7 @@ export default function DemoProgressBar() {
 </Stack>
 
 <Stack sx={{mt:{md:10, xs:5}}} direction="column" justifyContent="center" alignItems={{md:'flex-start', xs:'center'}}>
-  <Typography sx={{ml:{md:20}}} variant="h3"><a style={{color: "white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/smart">SMART Check:</a></Typography>
+  <Typography variant="h3"><a style={{color: "white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/smart">SMART Check:</a></Typography>
 <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
       <Typography style={{color:"white"}} variant="body1"> This method is particularly good at evaluating newspaper sources. Like the 5Ws method it also involves answering critical questions about your source. The criteria are:</Typography>
       <List sx={{ listStyleType: 'disc', ml: 4, mb: 2 }}>
@@ -104,7 +105,7 @@ export default function DemoProgressBar() {
 
 
 <Stack sx={{mt:{md:10, xs:5}}} direction="column" justifyContent="center" alignItems={{md:'flex-start', xs:'center'}}>
-<Typography sx={{ml:{md:20}}} variant="h3"><a style={{color: "white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/craap">CRAAP Test:</a></Typography>
+<Typography variant="h3"><a style={{color: "white", textDecoration: 'underline',textDecorationColor:'#52D17B',textUnderlineOffset:"7px"}} href="http://guides.lib.uw.edu/research/evaluate/craap">CRAAP Test:</a></Typography>
 <Container maxWidth="xl" sx={{mt: 2, px:2, py:3, bgcolor: '#353C42', borderRadius: 2}}>
       <Typography style={{color:"white"}} variant="body1"> This method provides you with a set of criteria that make a source more or less credible. The criteria are:</Typography>
       <List sx={{ listStyleType: 'disc', ml: 4, mb: 4 }}>
@@ -117,17 +118,18 @@ export default function DemoProgressBar() {
       </Container>
 </Stack>
 
-    <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{mr:{md:25}, mt:{md:5}}}>
+    <Stack direction="row" alignItems="center" justifyContent={{xs: "flext-start", md: "flex-end"}} sx={{mt:2}}>
       <Typography sx={{mb:2, color:'white'}} variant="body1">Source: <a style={{color: "#42a862", textDecoration: "none"}} href="https://guides.lib.uw.edu/research/faq/reliable">University of Washington</a></Typography>
     </Stack>
 
     <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
       <Typography style={{color:"white"}} variant="h6">Need more help?</Typography> 
       <Typography style={{color:"white"}} variant="h6">Contact our
-      <Button sx={{ml:{xs:0, sm:2}}} href="q&a/private/" variant="contained" color="green">
+      <Button sx={{ml:{xs:2, sm:2}}} href="q&a/private/" variant="contained" color="green">
         Specialists
       </Button></Typography> 
       </Stack>
+      </Container>
       </TabPanel>
 
 
