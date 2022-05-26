70a634bb684c1ba1c190295a2e16d04da50ff2ff
pero020 <petar.przic1@gmail.com>
2022-05-26 02:35:48 +0200

    add api header mods to getDomainData

--- pages/api/domainReviews/getDomainData/[url].js
+++ pages/api/domainReviews/getDomainData/[url].js
@@ -3,6 +3,7 @@ import clientPromise from "/lib/mongodb"
 export default async function handler (req, res) {
 
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
   
@@ -24,12 +25,12 @@ export default async function handler (req, res) {
       }
     ]).toArray();
     
+    
     domainData = domainData[0]
     if (!domainData) {
       res.status(404).send("Domain not found");
       return 0;
     }
-    
     res.status(200).json(domainData);
   } catch (e) {
     console.log(e)
